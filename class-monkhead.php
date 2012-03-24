311dde1fb376f23078f3bec0841e4365b4c73f8a
Ashfame <mail@ashfame.com>
2012-03-25 03:50:00 +0530

    Added functions to check for user existence, create a new user and create a new api key

--- class-monkhead.php
+++ class-monkhead.php
@@ -87,4 +87,65 @@ class Monkhead {
 		return $mysqli;
 	}
 
+	/**
+	 *	Function to check if the user already exists in the username
+	 */
+
+	public function user_exists( $user_email ) {
+		// Do a database query
+		$mysqli = $this->mysqli;
+		$query = "SELECT id FROM users WHERE email = '$user_email';";
+		$result = $mysqli->query( $query );
+		$row = mysqli_fetch_assoc( $result );
+
+		if ( is_null( $row ) )
+			return false;
+		else
+			return (int) $row['id'];
+	}
+
+	/**
+	 *	Function to create a new user
+	 */
+
+	public function create_user( $user_email ) {
+		// Write to database
+		$mysqli = $this->mysqli;
+		$query = "INSERT into users ( email ) VALUES ( '$user_email' );";
+		$result = $mysqli->query( $query );
+
+		// If successful, return user id
+		if ( $mysqli->insert_id )
+			return (int) $mysqli->insert_id;
+		else
+			return false;
+	}
+
+	/**
+	 *	Function to create new API key
+	 */
+
+	public function create_new_api_key( $user_email, $type = 'single', $label = '' ) {
+		// Fetch the user id
+		$user_id = $this->user_exists( $user_email );
+
+		// Create a new user if it doesn't exist
+		if ( ! $user_id )
+			$user_id = $this->create_user( $user_email );
+
+		// Generate API Key
+		$api_key = sha1( md5( sha1( $user_email ) . mt_rand( 345, 64634 ) . sha1( md5( $user_email ) ) ) );
+
+		// Write to database
+		$mysqli = $this->mysqli;
+		$query = "INSERT into api_keys ( user_id, api_key, type, service, label ) VALUES ( '$user_id', '$api_key', '$type', '$this->service', '$label' );";
+		$result = $mysqli->query( $query );
+
+		// If successful, return the newly generated api key
+		if ( $result )
+			return $api_key;
+		else
+			return false;
+	}
+
 }
