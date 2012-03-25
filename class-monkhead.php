94d9386ff422d682dc6b6c8fc8d3e2b69eb6a500
Ashfame <mail@ashfame.com>
2012-03-25 13:59:23 +0530

    Added function to get user details and a helper function to verify than an email is valid, the latter one taken from WordPress

--- class-monkhead.php
+++ class-monkhead.php
@@ -88,6 +88,61 @@ class Monkhead {
 	}
 
 	/**
+	 *	Function to verify that an email is valid.
+	 *
+	 *	Credit: WordPress
+	 */
+
+	public function is_email( $email ) {
+
+		// Test for the minimum length the email can be
+		if ( strlen( $email ) < 3 )
+			return false;
+
+		// Test for an @ character after the first position
+		if ( strpos( $email, '@', 1 ) === false )
+			return false;
+
+		// Split out the local and domain parts
+		list( $local, $domain ) = explode( '@', $email, 2 );
+
+		// LOCAL PART
+		// Test for invalid characters
+		if ( !preg_match( '/^[a-zA-Z0-9!#$%&\'*+\/=?^_`{|}~\.-]+$/', $local ) )
+			return false;
+
+		// DOMAIN PART
+		// Test for sequences of periods
+		if ( preg_match( '/\.{2,}/', $domain ) )
+			return false;
+
+		// Test for leading and trailing periods and whitespace
+		if ( trim( $domain, " \t\n\r\0\x0B." ) !== $domain )
+			return false;
+
+		// Split the domain into subs
+		$subs = explode( '.', $domain );
+
+		// Assume the domain will have at least two subs
+		if ( 2 > count( $subs ) )
+			return false;
+
+		// Loop through each sub
+		foreach ( $subs as $sub ) {
+			// Test for leading and trailing hyphens and whitespace
+			if ( trim( $sub, " \t\n\r\0\x0B-" ) !== $sub )
+				return false;
+
+			// Test for invalid characters
+			if ( !preg_match('/^[a-z0-9-]+$/i', $sub ) )
+				return false;
+		}
+
+		// Congratulations your email made it!
+		return true;
+	}
+
+	/**
 	 *	Function to check if the user already exists in the username
 	 */
 
@@ -119,6 +174,30 @@ class Monkhead {
 			return (int) $mysqli->insert_id;
 		else
 			return false;
+	}
+
+	/**
+	 *	Function to get user details
+	 *
+	 *	@param Integer (User ID) or String (User's email)
+	 */
+
+	public function get_user( $user ) {
+
+		if ( is_int( $user ) ) // If a user ID was passed
+			$query = 'SELECT * FROM users WHERE id = ' . (int) $user . ';';
+		else if ( $this->is_email( $user ) ) // If user's email was passed
+			$query = "SELECT * FROM users WHERE email = '$user';";
+		else // something else which is not an identifier
+			return false;
+
+		// Make a database query
+		$mysqli = $this->mysqli;
+		$result = $mysqli->query( $query );
+		$row = $result->fetch_assoc();
+
+		return $row;
+
 	}
 
 	/**
@@ -168,5 +247,5 @@ class Monkhead {
 		else
 			return false;
 	}
-	
+
 }
