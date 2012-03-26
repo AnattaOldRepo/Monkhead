888cc32d497d74c30a0b34e95a3c7122bda49e73
Ashfame <mail@ashfame.com>
2012-03-26 21:58:16 +0530

    Allow access to latest version status for Awesomecheckout without an API key

--- awesomecheckout-1alpha.php
+++ awesomecheckout-1alpha.php
@@ -10,17 +10,22 @@ class awesomecheckout_api_handler {
 	public function __construct() {
 		global $monkhead;
 
+		$need_to_verify_api_key = true;
+		
+		$api_endpoint_type = $monkhead->get_api_endpoint_type();
+		$api_endpoint_call = $monkhead->get_api_endpoint_call();
+		
+		if ( $api_endpoint_type == 'status' && $api_endpoint_call == 'latestVersion' )
+			$need_to_verify_api_key = false;
+		
 		// Verify API key
-		if ( ! $monkhead->verify_api_key() ) {
+		if ( $need_to_verify_api_key && ! $monkhead->verify_api_key() ) {
 			echo json_encode( array(
 				'status' => 'error',
 				'message' => 'Invalid Key!'
 			));
 			die();
 		}
-
-		$api_endpoint_type = $monkhead->get_api_endpoint_type();
-		$api_endpoint_call = $monkhead->get_api_endpoint_call();
 
 		// Call the API handler function
 		$this->serve( $api_endpoint_type, $api_endpoint_call );
