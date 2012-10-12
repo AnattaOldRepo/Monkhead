16daa161a50013abf6add207e076b52be6531b81
Ashfame <mail@ashfame.com>
2012-10-13 02:31:07 +0530

    formatting fix in awesomecheckout api file

--- awesomecheckout-1alpha.php
+++ awesomecheckout-1alpha.php
@@ -1,29 +1,28 @@
 <?php
 
 /**
- *	Product: AwesomeCheckout
- *	API Version: 1alpha
+ * 	Product: AwesomeCheckout
+ * 	API Version: 1alpha
  */
-
 class awesomecheckout_api_handler {
 
 	public function __construct() {
 		global $monkhead;
 
 		$need_to_verify_api_key = true;
-		
+
 		$api_endpoint_type = $monkhead->get_api_endpoint_type();
 		$api_endpoint_call = $monkhead->get_api_endpoint_call();
-		
+
 		if ( $api_endpoint_type == 'status' && $api_endpoint_call == 'latestVersion' )
 			$need_to_verify_api_key = false;
-		
+
 		// Verify API key
-		if ( $need_to_verify_api_key && ! $monkhead->verify_api_key() ) {
+		if ( $need_to_verify_api_key && !$monkhead->verify_api_key() ) {
 			echo json_encode( array(
 				'status' => 'error',
 				'message' => 'Invalid Key!'
-			));
+			) );
 			die();
 		}
 
@@ -45,7 +44,7 @@ class awesomecheckout_api_handler {
 			echo json_encode( array(
 				'status' => 'error',
 				'latestVersion' => 'Invalid API Endpoint!'
-			));
+			) );
 		}
 	}
 
@@ -53,7 +52,7 @@ class awesomecheckout_api_handler {
 		echo json_encode( array(
 			'status' => 'success',
 			'latestVersion' => '1.0'
-		));
+		) );
 		die();
 	}
 
