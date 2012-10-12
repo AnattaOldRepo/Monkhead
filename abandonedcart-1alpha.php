9a25c48ad49ed1adc4bad629f55b906402cfdf76
Ashfame <mail@ashfame.com>
2012-10-13 02:29:37 +0530

    improved allowed endpoints code & adding call for fetching latest version for abandoned carts

--- abandonedcart-1alpha.php
+++ abandonedcart-1alpha.php
@@ -12,7 +12,22 @@ class abandonedcart_api_handler {
 		$api_endpoint_type = $monkhead->get_api_endpoint_type();
 		$api_endpoint_call = $monkhead->get_api_endpoint_call();
 
-		if ( ( $api_endpoint_type == 'fetch' && $api_endpoint_call == 'payload' ) || ( $api_endpoint_type == 'collect' && $api_endpoint_call == 'ping' ) ) {
+		$allowed_endpoints = array(
+			'status' => 'latestVersion',
+			'fetch' => 'payload',
+			'collect' => 'ping'
+		);
+
+		$allow_call = false;
+
+		foreach ( $allowed_endpoints as $endpoint_type => $endpoint_call ) {
+			if ( $api_endpoint_type == $endpoint_type && $api_endpoint_call == $endpoint_call ) {
+				$allow_call = true;
+				break;
+			}
+		}
+
+		if ( $allow_call ) {
 			// Call the API handler function
 			$this->serve( $api_endpoint_type, $api_endpoint_call );
 		} else {
@@ -25,11 +40,21 @@ class abandonedcart_api_handler {
 	}
 
 	public function serve( $api_endpoint_type, $api_endpoint_call ) {
-		if ( $api_endpoint_type == 'fetch' && $api_endpoint_call == 'payload' ) {
+		if ( $api_endpoint_type == 'status' && $api_endpoint_call == 'latestVersion' ) {
+			$this->serve_latest_version();
+		} else if ( $api_endpoint_type == 'fetch' && $api_endpoint_call == 'payload' ) {
 			$this->serve_payload();
 		} else if ( $api_endpoint_type == 'collect' && $api_endpoint_call == 'ping' ) {
 			$this->collect_ping();
 		}
+	}
+
+	public function serve_latest_version() {
+		echo json_encode( array(
+			'status' => 'success',
+			'latestVersion' => '1.0'
+		) );
+		die();
 	}
 
 	public function serve_payload() {
