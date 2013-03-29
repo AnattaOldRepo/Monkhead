a42e5a14f81af30fec68789d93b94af45077a23e
Ashfame <mail@ashfame.com>
2013-03-30 00:37:24 +0530

    better gitpull file

--- gitpull.php
+++ gitpull.php
@@ -3,4 +3,4 @@
 mail( 'ashish@anattadesign.com', 'Monkhead - Github commits deployed', 'Commits were deployed on production' );
 mail( 'nirav@anattadesign.com', 'Monkhead - Github commits deployed', 'Commits were deployed on production' );
 
-`git pull`;
+echo `git pull`;
